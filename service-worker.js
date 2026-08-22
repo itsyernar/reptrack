31e7d69a44b92c5c737f34b3577a1741797ee9ed
itsyernar <ernarbatzhanov0@gmail.com>
2026-08-23 00:48:26 +0500

    Refresh offline cache for live workouts

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "reptrack-v5";
+const CACHE = "reptrack-v6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
